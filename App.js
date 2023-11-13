913b03c34f66621100139278c188a07fa486d80d
pabaill <philip.baillargeon@gmail.com>
2023-11-13 09:46:46 -0800

    Added icons to tabs

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import Home from './screens/Home';
 import Projects from './screens/Projects';
 import Friends from './screens/Friends';
 import Profile from './screens/Profile';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
+import { FontAwesome } from '@expo/vector-icons'; 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -14,10 +15,34 @@ export default function App() {
   return (
     <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Projects" component={Projects} />
-          <Tab.Screen name="Friends" component={Friends} />
-          <Tab.Screen name="Profile" component={Profile} />
+          <Tab.Screen name="Home" component={Home} options={{
+              tabBarLabel: "Home",
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome name="home" color={color} size={size} />
+              ),
+            }} 
+          />
+          <Tab.Screen name="Projects" component={Projects} options={{
+              tabBarLabel: "Projects",
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome name="search" color={color} size={size} />
+              ),
+            }} 
+          />
+          <Tab.Screen name="Friends" component={Friends} options={{
+              tabBarLabel: "Friends",
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome name="users" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen name="Profile" component={Profile} options={{
+              tabBarLabel: "Profile",
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome name="user" color={color} size={size} />
+              ),
+            }}
+          />
         </Tab.Navigator>
     </NavigationContainer>
   );
