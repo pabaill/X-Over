ad6c5d035699e4260432182bdbf499ce566b3aaf
pabaill <philip.baillargeon@gmail.com>
2023-11-17 14:46:36 -0800

    Styled Login w/ Logo

---
+++ assets/XOverTheme.js
@@ -0,0 +1,6 @@
+export default theme = {
+    base_orange: "#ec8a07",
+    base_yellow: "#edc207",
+    warn_blue: "#07e4ed",
+    bg_blue: "#3a4b6e",
+};
--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import { TextInput } from "react-native";
-import { View, Pressable, Text } from "react-native";
+import { View, Pressable, Text, SafeAreaView, Image } from "react-native";
 import { StyleSheet } from "react-native-web";
+
+import { useFonts, Kanit_400Regular } from "@expo-google-fonts/kanit";
 
 import {auth} from './../firebase';
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+
+import theme from "./../assets/XOverTheme";
 
 export default function Login() {
     const [email, setEmail] = useState('');
@@ -24,23 +28,62 @@ export default function Login() {
         }).catch(err => alert(err.message));
     };
 
+    let [fontsLoaded] = useFonts({
+        Kanit_400Regular,
+      });
+    
+      if (!fontsLoaded) {
+        return null;
+      }
+
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Welcome to X-Over</Text>
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <TextInput placeholder="Email" value={email} onChangeText={s => setEmail(s)} />
-            <TextInput placeholder="Password" value={password} onChangeText={s => setPassword(s)} />
+      <SafeAreaView style={{ marginTop: 100, flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={styles.titleText}>Welcome to</Text>
+        <Image alt="X-Over" source={require('./../assets/X-Over-Logo.png')} />
+        <View style={{ flex: 1, justifyContent: 'space-around', alignItems: 'center' }}>
+            <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={s => setEmail(s)} />
+            <TextInput style={styles.input} placeholder="Password" value={password} onChangeText={s => setPassword(s)} />
         </View>
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Pressable onPress={handleLogIn}><Text>Log In</Text></Pressable>
-            <Pressable onPress={handleSignUp} ><Text>Sign Up</Text></Pressable>
+        <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center'}}>
+            <Pressable style={[styles.pressable, styles.shadowProp]} onPress={handleLogIn}><Text style={styles.baseText}>Log In</Text></Pressable>
+            <Pressable style={[styles.pressable, styles.shadowProp]} onPress={handleSignUp} ><Text style={styles.baseText}>Sign Up</Text></Pressable>
         </View>
-      </View>
+      </SafeAreaView>
     );
   }
 
-// const styles = StyleSheet.create({
-//     baseText: {
-//         fontFamily: "Kanit"
-//     }
-// })
+const styles = StyleSheet.create({
+    pressable: {
+        backgroundColor: theme.base_yellow,
+        width: 300,
+        margin: 40,
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 30
+    },
+    input: {
+        fontFamily: "Kanit_400Regular",
+        borderStyle: "solid",
+        borderWidth: 1,
+        width: 300,
+        height: 50,
+        margin: "auto",
+        padding: 10
+    },
+    titleText: {
+        fontFamily: "Kanit_400Regular",
+        fontSize: 40
+    },
+    baseText: {
+        fontFamily: "Kanit_400Regular",
+        fontSize: 24
+    },
+    shadowProp: {
+        shadowColor: '#000000',
+        shadowOffset: {width: -2, height: 4},
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        elevation: 5
+      }
+})
