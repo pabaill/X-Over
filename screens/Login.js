5fb5ee4918b8939e1159247d0d29118b8bdc2af2
pabaill <philip.baillargeon@gmail.com>
2023-11-17 14:53:54 -0800

    Password Entry and fixed keyboard glitch

--- screens/Login.js
+++ screens/Login.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { TextInput } from "react-native";
-import { View, Pressable, Text, SafeAreaView, Image } from "react-native";
+import { View, Pressable, Text, SafeAreaView, Image, ScrollView } from "react-native";
 import { StyleSheet } from "react-native-web";
 
 import { useFonts, Kanit_400Regular } from "@expo-google-fonts/kanit";
@@ -38,16 +38,18 @@ export default function Login() {
 
     return (
       <SafeAreaView style={{ marginTop: 100, flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ScrollView  > 
         <Text style={styles.titleText}>Welcome to</Text>
         <Image alt="X-Over" source={require('./../assets/X-Over-Logo.png')} />
         <View style={{ flex: 1, justifyContent: 'space-around', alignItems: 'center' }}>
             <TextInput style={styles.input} placeholder="Email" value={email} onChangeText={s => setEmail(s)} />
-            <TextInput style={styles.input} placeholder="Password" value={password} onChangeText={s => setPassword(s)} />
+            <TextInput secureTextEntry={true} style={styles.input} placeholder="Password" value={password} onChangeText={s => setPassword(s)} />
         </View>
-        <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center', marginTop: 50}}>
             <Pressable style={[styles.pressable, styles.shadowProp]} onPress={handleLogIn}><Text style={styles.baseText}>Log In</Text></Pressable>
             <Pressable style={[styles.pressable, styles.shadowProp]} onPress={handleSignUp} ><Text style={styles.baseText}>Sign Up</Text></Pressable>
         </View>
+        </ScrollView>
       </SafeAreaView>
     );
   }
@@ -56,7 +58,8 @@ const styles = StyleSheet.create({
     pressable: {
         backgroundColor: theme.base_yellow,
         width: 300,
-        margin: 40,
+        height: 50,
+        margin: 30,
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
@@ -68,7 +71,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         width: 300,
         height: 50,
-        margin: "auto",
+        margin: 10,
         padding: 10
     },
     titleText: {
