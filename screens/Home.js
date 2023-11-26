a0884aba0fd56d916c571a901ab248aee3d12da4
pabaill <philip.baillargeon@gmail.com>
2023-11-25 20:29:40 -0800

    Initial preview scroll implementation

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 
 import SegmentedControl from '@react-native-segmented-control/segmented-control';
 import XOverTheme from '../assets/XOverTheme';
@@ -8,8 +8,12 @@ import { useFonts, Kanit_400Regular, Kanit_700Bold } from "@expo-google-fonts/ka
 
 import { BlurView } from 'expo-blur';
 
+import Carousel from 'react-native-reanimated-carousel';
+
 
 export default function Home() {
+
+  const width = Dimensions.get('window').width;
 
   const [selectedIndex, changeIndex] = useState(0);
 
@@ -21,8 +25,16 @@ export default function Home() {
     return null;
   }
 
+  _renderItem = ({item, index}) => {
+    return (
+        <View>
+            <Text>{ item.title }</Text>
+        </View>
+    );
+}
+
   return (
-    <View>
+    <View style={{padding: 20}}>
       <SegmentedControl
           style={{marginTop: 40, width: "80%", marginLeft: "10%", marginRight: "10%"}}
           values={['Recent', 'Pinned']}
@@ -36,7 +48,7 @@ export default function Home() {
         />
         {selectedIndex === 0 ? (
           <View>
-            <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-around", marginTop: 20}}>
+            {/* <View style={{flexDirection: "row", alignItems: "center", justifyContent: "space-around", marginTop: 20}}>
               <BlurView intensity={1} tint='dark' style={[styles.blurred, styles.project_prev]}>
                 <Text style={{textAlign: "center"}}>Left</Text>
               </BlurView>
@@ -46,9 +58,35 @@ export default function Home() {
               <BlurView intensity={1} tint='dark' style={[styles.blurred, styles.project_prev]}>
                 <Text style={{textAlign: "center"}}>Right</Text>
               </BlurView>
-            </View>
+            </View> */}
+            <Carousel
+                loop
+                width={width}
+                height={width / 2}
+                autoPlay={true}
+                data={[...new Array(6).keys()]}
+                scrollAnimationDuration={1000}
+                onSnapToItem={(index) => console.log('current index:', index)}
+                renderItem={({ index }) => (
+                    <View
+                        style={{
+                            flex: 1,
+                            borderWidth: 1,
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
+                            {index}
+                        </Text>
+                    </View>
+                )}
+            />
             <Image style={{width: "auto", height: 40}} source={require('./../assets/X-Over-Drawer.png')} />
-            <Text style={styles.header}>Updates: </Text>
+            <View style={styles.shadow} >
+              <View style={styles.headerWrapper}>
+                <Text style={styles.header}>Updates: </Text>
+              </View>
+            </View>
             <View style={{flexDirection: "column", marginTop: 10, height: 400}}>
               <View style={styles.bubbleWrapper}>
                 <Image style={styles.bubble} source={require("./../assets/X-Over-Bubble.png")} />
@@ -72,5 +110,19 @@ const styles = StyleSheet.create({
   bubble: {width: "auto", height: 70},
   blurred: { flex: 2 },
   middle: {flex: 3},
-  header: {fontFamily: "Kanit_700Bold", fontSize: 30, borderColor: "black", borderWidth: 3, backgroundColor: XOverTheme.base_orange, width: 145, marginLeft: 10, padding: 5}
+  header: {
+    fontFamily: "Kanit_700Bold", 
+    fontSize: 30
+  },
+  headerWrapper: {
+    borderColor: "black", 
+    borderWidth: 3, 
+    backgroundColor: XOverTheme.base_orange, 
+    width: 145, 
+    marginLeft: 10, 
+    padding: 5,
+    top: -5,
+    left: -5
+  },
+  shadow: {backgroundColor: "black", width: 145}
 });
