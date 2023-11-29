d72924dc5d98f7e5dd063312192a8ad6a2efa57c
JackRyanMorris <morris11@stanford.edu>
2023-11-29 14:20:48 -0800

    Update Profile.js
    working on profile

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,63 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-// Styles
-const styles = {
-  app: {
-    flex: 4,
-    marginHorizontal: "auto",
-    width: 400,
-    backgroundColor: "#fff"
-  },
-  row: {
-    flexDirection: "row"
-  },
-  "1col": { 
-    backgroundColor: "#264d73", //blue color
-    borderColor: "#fff",
-    borderWidth: 1,
-    flex: 4
-  },
-  "2col": {
-    backgroundColor: "#fff",
-    borderColor: "#fff",
-    borderWidth: 1,
-    flex: 1
-  },
-  "3col": {
-    flex: 4
-  }
-};
-
-// RN Code
-const Col = ({ numRows, children }) => {
-  return (
-    <View style={styles[`${numRows}col`]}>{children}</View>
-  )
-}
-
-const Row = ({ children }) => (
-  <View style={styles.row}>{children}</View>
-)
-
 export default function Profile() {
   return (
-    <View style={styles.app}>
-      <Row>
-        <Col numRows={1}>
-          <Text>First column</Text>
-        </Col>
-        <Col numRows={1}>
-          <Text>Second column</Text>
-        </Col>
-      </Row>
-      <Row>
-        <Col numRows={1}>
-          <Text>First column</Text>
-        </Col>
-        <Col numRows={1}>
-          <Text>Second Column</Text>
-        </Col>
-      </Row>
+    <View style={{height: '100%'}}>
+      <View
+        sytle={{
+          height: '15%',
+          backgroundColor: 'powderblue',
+        }}
+      />
+      <View
+        style={{
+          width: '66%',
+          height: '35%',
+          backgroundColor: 'skyblue',
+        }}
+      />
+      <View
+        style={{
+          width: '33',
+          height: '50%',
+          backgroundColor: 'steelblue',
+        }}
+      />
     </View>
   )
 }
