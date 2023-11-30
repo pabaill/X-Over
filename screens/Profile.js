e436bb1ea456136569d646fdde135f512b970fde
JackRyanMorris <morris11@stanford.edu>
2023-11-29 21:32:36 -0800

    Update Profile.js
    Update to Profile

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,29 +1,37 @@
 import { StyleSheet, Text, View } from 'react-native';
 import XOverTheme from '../assets/XOverTheme';
+import {useState} from 'react';
 
 export default function Profile() {
   return (
-    <View style={{height: '100%'}}>
-      <View
-        style={{
-          height: '10%',
-          width: '100%',
-          backgroundColor: XOverTheme.bg_blue,
-        }}
-      />
-      <View
-        style={{
-          height: '4%',
-          width: '100%',
-        }}
-      />
-      <View
-        style={{
-          height: '19%',
-          width: '100%',
-          backgroundColor: XOverTheme.bg_blue,
-        }}
-      />
+    <View style={{flex: 1}}>
+      <View style={{flex: 7, backgroundColor: XOverTheme.bg_blue}} />
+      <View style={{flex: 10, flexDirection: 'row'}}>
+        <Text style={styles.settingsText}>Settings</Text>
+        <Text style={styles.profileText}>Profile</Text>
+        <Text style={styles.settingsText}>Logout</Text>
+      </View>
+      <View style={{flex: 16, backgroundColor: XOverTheme.bg_blue}} />
+      <View style={{flex: 67}} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  profileText: {
+    flex: 5,
+    textAlign: 'center',
+    fontSize: 30,
+    color: 'white',
+    backgroundColor: XOverTheme.bg_blue,
+    fontFamily: "Kanit_400Regular"
+  },
+  settingsText: {
+    flex: 5,
+    fontSize: 15,
+    color: 'white',
+    backgroundColor: XOverTheme.bg_blue,
+    textAlign: 'center',
+    fontFamily: "Kanit_400Regular"
+  }
+});
