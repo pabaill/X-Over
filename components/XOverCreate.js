52630ad4d199ce96f20bd8e86f1dbff6f7329990
pabaill <philip.baillargeon@gmail.com>
2023-12-01 18:38:35 -0800

    Create XOver first screen keyboard

--- components/XOverCreate.js
+++ components/XOverCreate.js
@@ -1,20 +1,36 @@
-import { Text, View, StyleSheet, Pressable } from "react-native";
+import { Text, View, StyleSheet, Pressable, KeyboardAvoidingView, SafeAreaView, Image } from "react-native";
 import { useState } from "react";
 import XOverButton from "./XOverButton";
 import XOverTheme from "../assets/XOverTheme";
 import { FontAwesome } from "@expo/vector-icons";
 import { useFonts } from "expo-font";
 import { Kanit_400Regular } from "@expo-google-fonts/kanit";
-import { TextInput } from "react-native-gesture-handler";
+import { ScrollView, TextInput } from "react-native-gesture-handler";
 import * as DocumentPicker from 'expo-document-picker';
 
 export default function XOverCreate({setCreateModal}) {
 
     const [pageNum, changePageNum] = useState(0);
+    const [pendingProj, updatePendingProj] = useState({
+        name: "",
+        description: "",
+        thumb: "",
+        members: [],
+        isPublic: false,
+        tags: []
+    })
 
     const selectPDF = async () => {
         try {
           const result = await DocumentPicker.getDocumentAsync();
+          updatePendingProj({
+            name: pendingProj.name, 
+            description: pendingProj.description, 
+            thumb: result, 
+            members: pendingProj.members, 
+            isPublic: pendingProj.isPublic, 
+            tags: pendingProj.tags
+        })
           console.log(
             result.uri,
             result.type, // mime type
@@ -39,42 +55,65 @@ export default function XOverCreate({setCreateModal}) {
     }
 
     return (
-        <View style={{flex: 1, width: "100%", height: "100%", padding: 20, alignItems: "center", justifyContent: "center", backgroundColor: XOverTheme.bg_blue + "d0"}}>
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: XOverTheme.bg_blue + "d0"}}>
             {pageNum === 0 && 
-            <View style={{width: "100%", marginHorizontal: "5%", height: "90%", backgroundColor: "white", borderRadius: 15, padding: 10}}>
-                <XOverButton icon={(<FontAwesome style={{fontSize: 32 }} name="arrow-left" />)} pressFunc={() => {setCreateModal(false)}} />
-                <View style={{flex: 9, alignItems: "center", justifyContent: "space-around", height: "90%"}}>
-                    <View style={styles.infoContainer}>
-                        <Text style={styles.infoText}>Create Your X-Over</Text>
+            // <View style={{position: "absolute", flex: 1, alignItems: "center", justifyContent: "center", width: "100%", marginHorizontal: "5%", height: "100%", backgroundColor: "white", borderRadius: 15, padding: 10}}>
+            //     <XOverButton icon={(<FontAwesome style={{fontSize: 32 }} name="arrow-left" />)} pressFunc={() => {setCreateModal(false)}} />
+            //     <View style={{flex: 9, alignItems: "center", justifyContent: "space-around", height: "90%", width: "100%"}}>
+            //         <View style={styles.infoContainer}>
+            //             <Text style={styles.infoText}>Create Your X-Over</Text>
+            //         </View>
+            //         <TextInput placeholderTextColor={"white"} style={styles.input} placeholder="Project Name" />
+            //         <TextInput multiline numberOfLines={6} placeholderTextColor={"white"} style={[styles.input, styles.description]} placeholder="Description" />
+            //         <View style={styles.fileBrowseContainer} >
+            //             <Pressable onPress={selectPDF}>
+            //                 <FontAwesome style={{fontSize: 44, color: "white", alignSelf: "center" }} name="photo" />
+            //                 <Text style={styles.infoText}>Choose Project Thumbnail Image</Text>
+            //             </Pressable>
+            //         </View>
+            //     </View>
+            //     <View style={{flex: 1, alignSelf: "flex-end", marginRight: 20}}>
+            //         <XOverButton text={"Next"} pressFunc={() => {changePageNum(pageNum + 1)}} />
+            //     </View>
+            // </View>
+            <SafeAreaView style={{ marginVertical: 40, flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "white", minHeight: "80%", borderRadius: 15, minWidth: "90%" }}>
+                <ScrollView style={{flex: 1}}  >
+                    <XOverButton containerStyles={{margin: 10}} icon={(<FontAwesome style={{fontSize: 32 }} name="arrow-left" />)} pressFunc={() => {setCreateModal(false)}} />
+                    <View style={{flex: 1, alignItems: "center", justifyContent: "space-around", height: "100%", width: "100%"}}>
+                        <View style={[styles.infoContainer, {marginTop: 20, marginBottom: 60}]}>
+                            <Text style={styles.infoText}>Create Your X-Over</Text>
+                        </View>
+                        <TextInput placeholderTextColor={"white"} style={[styles.input, {marginBottom: 40}]} placeholder="Project Name" />
+                        <TextInput multiline numberOfLines={6} placeholderTextColor={"white"} style={[styles.input, styles.description, {marginBottom: 60}]} placeholder="Description" />
+                        <View style={[styles.fileBrowseContainer, {marginBottom: 30}]} >
+                            <Pressable onPress={selectPDF}>
+                                <FontAwesome style={{fontSize: 44, color: "white", alignSelf: "center" }} name="photo" />
+                                <Text style={styles.infoText}>Choose Project Thumbnail Image</Text>
+                            </Pressable>
+                        </View>
                     </View>
-                    <TextInput placeholderTextColor={"white"} style={styles.input} placeholder="Project Name" />
-                    <TextInput multiline numberOfLines={6} placeholderTextColor={"white"} style={[styles.input, styles.description]} placeholder="Description" />
-                    <View style={styles.fileBrowseContainer} >
-                        <Pressable onPress={selectPDF}>
-                            <FontAwesome style={{fontSize: 44, color: "white", alignSelf: "center" }} name="photo" />
-                            <Text style={styles.infoText}>Choose Project Thumbnail Image</Text>
-                        </Pressable>
+                    <View style={{flex: 1, alignSelf: "flex-end", marginRight: 20}}>
+                        <XOverButton text={"Next"} pressFunc={() => {changePageNum(pageNum + 1)}} />
                     </View>
-                </View>
-                <View style={{flex: 1, alignSelf: "flex-end", marginRight: 20}}>
-                    <XOverButton text={"Next"} pressFunc={() => {changePageNum(pageNum + 1)}} />
-                </View>
-            </View>}
+                </ScrollView>
+            </SafeAreaView>
+            }
             {pageNum === 1 &&
-            <View style={{width: "100%", marginHorizontal: "5%", height: "90%", backgroundColor: "white", borderRadius: 15, padding: 10}}>
+            <ScrollView style={{width: "100%", marginHorizontal: "5%", height: "90%", backgroundColor: "white", borderRadius: 15, padding: 10}}>
                 <XOverButton text={"Back"} pressFunc={() => {changePageNum(pageNum - 1)}} />
                 <XOverButton text={"Next"} pressFunc={() => {changePageNum(pageNum + 1)}} />
-            </View>
+            </ScrollView>
             }
         </View>
-    );
+        );
 }
 
 const styles = StyleSheet.create({
     infoContainer: {
-        backgroundColor: XOverTheme.bg_blue + "d0",
-        width: "100%",
-        height: "10%",
+        backgroundColor: XOverTheme.bg_blue + "a0",
+        width: "90%",
+        marginHorizontal: "5%",
+        height: "15%",
         justifyContent: "center",
         borderRadius: 50
     },
@@ -96,14 +135,14 @@ const styles = StyleSheet.create({
         backgroundColor: XOverTheme.bg_blue,
         borderRadius: 15,
         alignItems: "center",
-        justifyContent: "space-evenly"
+        justifyContent: "center"
       },
     description: {
     },
     fileBrowseContainer: {
         backgroundColor: XOverTheme.bg_blue,
         width: "90%",
-        marginHorizontal: "10%",
+        marginHorizontal: "5%",
         height: "30%",
         borderRadius: 15,
         alignItems: "center",
