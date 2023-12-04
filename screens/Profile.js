2587d8a87e37d04b2d71bb63fa680bf582043d94
JackRyanMorris <morris11@stanford.edu>
2023-12-03 16:01:50 -0800

    Update Profile.js
    adjustments

--- screens/Profile.js
+++ screens/Profile.js
@@ -11,8 +11,16 @@ export default function Profile() {
         <Text style={styles.profileText}>Profile</Text>
         <Text style={styles.settingsText}>Logout</Text>
       </View>
+<<<<<<< Updated upstream
       <Image source={require("./../assets/default_profile.png")} style={{flex: 16, borderWidth: 1, borderColor: 'white', backgroundColor: XOverTheme.bg_blue}} />
       <View style={{flex: 16, backgroundColor: XOverTheme.bg_blue}} />
+=======
+      <View style={{flex: 16, flexDirection: 'row'}}>
+        <View style={{flex: 5, backgroundColor: XOverTheme.bg_blue}} />
+        <Image style={{flex: 5, width: 20, backgroundColor: XOverTheme.bg_blue}} source={require("./../assets/default_profile.png")} />
+        <View style={{flex: 5, backgroundColor: XOverTheme.bg_blue}} />
+      </View>
+>>>>>>> Stashed changes
       <View style={{flex: 67}} />
     </View>
   );
