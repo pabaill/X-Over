f1f1743b6eaa78ed004b2e3668b3c0628773c5f5
JackRyanMorris <morris11@stanford.edu>
2023-12-05 17:43:30 -0800

    adjustments

--- assets/XOverFriendsInfo.js
+++ assets/XOverFriendsInfo.js
@@ -1,111 +1,111 @@
 export default FriendsInfo = {
     person1: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic1.png'),
         name: 'Steve',
         title: 'Mechanical Engineer',
         phone: '575-495-3414',
         email: 'stevemerrick@gmail.com'
     },
     person2: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic5.png'),
         name: 'Natasha',
         title: 'Head of Engineering',
         phone: '310-667-1281',
         email: 'njuarez@gmail.com'
     },
     person3: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic2.png'),
         name: 'Tony',
         title: 'Mechanical Engineer',
         phone: '551-966-1328',
         email: 'tonlloyd@gmail.com'
     },
     person4: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic10.png'),
         name: 'Bruce',
         title: 'Computer Engineer',
         phone: '256-314-2057',
         email: 'brl2@gmail.com'
     },
     person5: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic6.png'),
         name: 'Kevin',
         title: 'Mechanical Engineer',
         phone: '351-669-3889',
         email: 'kevinlarken13@gmail.com'
     },
     person6: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic7.png'),
         name: 'Olivia',
         title: 'Computer Engineer',
         phone: '435-296-7771',
         email: 'liv1988@gmail.com'
     },
     person7: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic3.png'),
         name: 'Michael',
         title: 'Chemical Engineer',
         phone: '283-297-6395',
         email: 'michaelsinbox@gmail.com'
     },
     person8: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic11.png'),
         name: 'Megan',
         title: 'Human Relations',
         phone: '840-919-1497',
         email: 'megthereg@gmail.com'
     },
     person9: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic9.png'),
         name: 'Jin',
         title: 'President',
         phone: '276-213-3665',
         email: 'jincross@gmail.com'
     },
     person10: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic4.png'),
         name: 'Miguel',
         title: 'Head of Marketing',
         phone: '838-482-6674',
         email: 'miggypiggy@gmail.com'
     },
     person11: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic12.png'),
         name: 'Bill',
         title: 'Lead on iPhone25',
         phone: '667-544-1314',
         email: 'twentydollarbill@gmail.com'
     },
     person12: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic7.png'),
         name: 'Ted',
         title: 'Software Engineer',
         phone: '346-830-2064',
         email: 'ted2@gmail.com'
     },
     person13: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic8.png'),
         name: 'John',
         title: 'Lead on Google Pixel 12',
         phone: '352-473-2135',
         email: 'hancockjohn@gmail.com'
     },
     person14: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic2.png'),
         name: 'Mary',
         title: 'Software Engineer',
         phone: '855-957-6849',
         email: 'bloodymary@gmail.com'
     },
     person15: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic9.png'),
         name: 'Alice',
         title: 'Lead on Surface XL14',
         phone: '563-475-8593',
         email: 'wonderland@gmail.com'
     },
     person16: {
-        image: require('./../assets/default_profile.png'),
+        image: require('./../assets/ProfilePics/Profile-Pic2.png'),
         name: 'Jimmy',
         title: 'Visual Designer',
         phone: '312-803-6389',
