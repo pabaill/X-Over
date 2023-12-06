5f8b4f8e14bcfc3d68a330e6fc9160fa91f4a21e
pabaill <philip.baillargeon@gmail.com>
2023-12-05 21:28:25 -0800

    Search list popup

--- components/XOverFriend.js
+++ components/XOverFriend.js
@@ -1,28 +1,30 @@
 import { useFonts, Kanit_400Regular, Kanit_700Bold } from 'expo-font';
-import { Pressable, View, Text, Image, StyleSheet } from 'react-native';
+import { Pressable, View, Text, Image, StyleSheet, Alert } from 'react-native';
 import XOverTheme from '../assets/XOverTheme';
 
-export default function XOverFriend({image, name, roles}) {
+export default function XOverFriend({friend, setFriendToShow}) {
     return (
+        <Pressable onPress={() => {setFriendToShow(friend)}}>
         <View style={{height: 95, flexDirection: 'column', width: "100%"}}>
             <View style={{height: 20, width: "100%"}}/>
             <View style={{height: 55, width: "100%"}}>
-                <Pressable onPress={() => {}} style={{flex: 1, flexDirection: 'row', width: "100%", height: 80}} >
+                <View style={{flex: 1, flexDirection: 'row', width: "100%", height: 80}} >
                     <View style={{flex: 8}}/>
-                    <Image style={{flex: 17, height: '100%', width: '14%', borderColor: 'white'}} source={image} />
+                    <Image style={{flex: 17, height: '100%', width: '14%', borderColor: 'white'}} source={friend.image} />
                     <View style={{flex: 2}}/>
                     <View style={{flex: 45, flexDirection: 'column'}}>
                         <View style={{flex: 1}}/>
-                        <Text style={{flex: 3, fontSize: 15}}>{name}</Text>
+                        <Text style={{flex: 3, fontSize: 15}}>{friend.name}</Text>
                         <View style={{flex: 1}}/>
-                        <Text numberOfLines={1} style={{flex: 3,fontSize: 15}}>{roles.join(", ")}</Text>
+                        <Text numberOfLines={1} style={{flex: 3,fontSize: 15}}>{friend.role.join(", ")}</Text>
                         <View style={{flex: 1}}/>
                     </View>
                     <View style={{flex: 20}}/>
                     <View style={{flex: 8}}/>
-                </Pressable>
+                </View>
             </View>
             <View style={{height: 20}}/>
         </View>
+        </Pressable>
     );
 }
--- screens/Friends.js
+++ screens/Friends.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View, ImageBackground, ScrollView, Pressable } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, ScrollView, Image, Modal } from 'react-native';
 import SegmentedControl from '@react-native-segmented-control/segmented-control';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import XOverTheme from '../assets/XOverTheme';
 import XOverFriend from '../components/XOverFriend';
 import XOverSearch from '../components/XOverSearch';
 import XOverFriendsInfo from '../assets/XOverFriendsInfo';
+import XOverButton from '../components/XOverButton';
+import { FontAwesome } from '@expo/vector-icons';
 import { FlatList, TextInput } from 'react-native-gesture-handler';
 import PROJ_DATA from './../assets/mock_data';
 
@@ -12,6 +14,9 @@ export default function Friends({navigation}) {
   const [selectedIndex, changeIndex] = useState(0);
   
   const [searchPhrase, setSearchPhrase] = useState("");
+  const [clicked, setClicked] = useState(false);
+
+  const [friendToShow, setFriendToShow] = useState(null);
   // const getRelevantProjects = () => {
   //   return PROJ_DATA.filter((project) => project.name.toLowerCase().includes(searchPhrase.toLowerCase()) || project.tags.filter((tag) => tag.toLowerCase().includes(searchPhrase.toLowerCase())).length > 0);
   // }
@@ -26,19 +31,42 @@ export default function Friends({navigation}) {
       project.members.forEach((m) => {
         if (!union.find((x) => x.email === m.email)) {
           m.role = [m.role.toString()];
+          m.project = [project.name];
           union.push(m);
         } else {
           union[union.findIndex((x) => x.email === m.email )].role.push(m.role.toString());
+          union[union.findIndex((x) => x.email === m.email )].project.push(project.name);
         }
       })
     })
-    console.log(union)
     return union.sort((a, b) => a.name - b.name);
   };
   
   
   return (
     <ImageBackground imageStyle={{opacity: .1}} style={{height: "100%"}} source={require('./../assets/comic_dots.jpg')} >
+    <Modal 
+        animationType='fade'
+        transparent={true}
+        visible={friendToShow?.name !== undefined}
+        onRequestClose={() => {
+          setFriendToShow(null);
+        }}
+        >
+          <View style={{flex: 1, width: "100%", height: "100%", padding: 10, alignItems: "center", justifyContent: "center", backgroundColor: XOverTheme.bg_blue + "d0"}}>
+            <ScrollView contentContainerStyle={{alignItems: "flex-start", justifyContent: "flex-start", height: "100%"}} style={{ flex: 1, backgroundColor: "white", width: "90%", marginVertical: "50%", borderRadius: 25, padding: 20}}>
+              <XOverButton containerStyles={{marginTop: 20}} icon={(<FontAwesome name="arrow-left" style={{fontSize: 32 }} />)} pressFunc={() => {setFriendToShow(null)}} />
+                <View style={{width: "100%", height: 80, flexDirection: "row", alignItems: "center", marginTop: 20}}>
+                  <Image style={{height: 80, width: 80}} source={friendToShow?.image} />
+                  <View style={{marginLeft: 20}}>
+                    <Text style={{fontFamily: "Kanit_400Regular", fontSize: 24 }}>{friendToShow?.name}</Text>
+                    <Text numberOfLines={2} style={{fontFamily: "Kanit_400Regular", fontSize: 18, width: "100%" }}>{friendToShow?.role} from {friendToShow?.project}</Text>
+                  </View>
+                </View>
+              <Text style={{fontFamily: "Kanit_400Regular", fontSize: 18, alignSelf: "center", marginTop: "15%"}} >Contact me at my email if you'd like to collaborate: {friendToShow?.email}</Text>
+            </ScrollView>
+          </View>
+        </Modal>
     <View style={{ flex: 1, padding: 20}}>
       <SegmentedControl
             style={{marginTop: 40, width: "80%", marginLeft: "10%", marginRight: "10%"}}
@@ -51,14 +79,14 @@ export default function Friends({navigation}) {
               changeIndex(event.nativeEvent.selectedSegmentIndex);
             }}
           />
-      <TextInput value={searchPhrase} onChangeText={(text) => {setSearchPhrase(text)}} placeholderTextColor={"white"} style={styles.input} placeholder='Search' />
+      <XOverSearch searchPhrase={searchPhrase} setSearchPhrase={setSearchPhrase} clicked={clicked} setClicked={setClicked} />
       {selectedIndex === 0 ? (
       <ScrollView style={{padding: 10}}>
         <FlatList
           data={getMembers()}
-          renderItem={({item, index}) => (
-            <XOverFriend image={item.image} name={item.name} roles={item.role} />
-          )}
+          renderItem={({item, index}) => item.name.toLowerCase().includes(searchPhrase.toLowerCase()) ? (
+            <XOverFriend friend={item} setFriendToShow={setFriendToShow} />
+          ) : (<></>)}
           />
         {/* <XOverFriend image={XOverFriendsInfo.person9.image} name={XOverFriendsInfo.person9.name} title={XOverFriendsInfo.person9.title} />
         <XOverFriend image={XOverFriendsInfo.person10.image} name={XOverFriendsInfo.person10.name} title={XOverFriendsInfo.person10.title} />
@@ -73,9 +101,9 @@ export default function Friends({navigation}) {
         <ScrollView style={{padding: 10}}>
           <FlatList
             data={getMembers()}
-            renderItem={({item, index}) => (
-              <XOverFriend image={item.image} name={item.name} roles={item.role} />
-            )}
+            renderItem={({item, index}) => item.name.toLowerCase().includes(searchPhrase.toLowerCase()) ? (
+              <XOverFriend friend={item} setFriendToShow={setFriendToShow} />
+            ) : (<></>)}
           />
       </ScrollView>
       )}
